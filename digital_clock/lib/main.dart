e8e622cd3f0e9e2aff4b4e8bba3a9282f7fcac97
Prince Junior Mguni <prince.mguni@outlook.com>
2021-11-10 11:29:05 +0200

    updated pubspec

--- digital_clock/lib/main.dart
+++ digital_clock/lib/main.dart
@@ -4,16 +4,15 @@
 
 import 'dart:io';
 
-import 'package:flutter_clock_helper/customizer.dart';
-import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/customizer.dart';
+import 'package:flutter_clock_helper/model.dart';
 
 import 'digital_clock.dart';
 
 void main() {
   if (!kIsWeb && Platform.isMacOS) {
-    // TODO(gspencergoog): Update this when TargetPlatform includes macOS.
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
 
